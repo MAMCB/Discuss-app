f164135d7929129841f668b7fb0773bf60be6b6c
MAMCB <96302464+MAMCB@users.noreply.github.com>
2024-05-14 19:14:00 +0200

    tested authentication from server and from client

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,23 @@
 import { Button } from "@nextui-org/react"
+import * as actions from "@/actions"
+import {auth} from "@/auth"
+import Profile from "@/components/Profile";
 
-export default function Home() {
+export default async function Home() {
+  const session = await auth();
   return (
-   <div>
-    <h1>Home</h1>
-    <Button>Click me</Button>
-   </div>
-  )
+    <div>
+      <form action={actions.signIn}>
+        <Button type="submit">Sign in</Button>
+      </form>
+      <form action={actions.signOut}>
+        <Button type="submit">Sign Out</Button>
+      </form>
+
+      {
+        session?.user ? (<div>Signed In: {JSON.stringify(session.user)}</div>) : (<div>Not Signed In</div>)
+      }
+      <Profile />
+    </div>
+  );
 }
---
+++ src/components/Profile.tsx
@@ -0,0 +1,25 @@
+'use client';
+import { useSession } from 'next-auth/react';
+
+
+const Profile = () => {
+    const session = useSession();
+    if(session.data?.user)
+        {
+            return (
+                <div>
+                    From client:user is signed in
+                    <h1>Profile</h1>
+                    <p>{session.data.user.name}</p>
+                    <p>{session.data.user.email}</p>
+                    <img src={session.data.user.image!} alt="user image" />
+                    
+                </div>
+            )
+        }
+  return (
+    <div>From client:user is NOT signed in</div>
+  )
+}
+
+export default Profile
