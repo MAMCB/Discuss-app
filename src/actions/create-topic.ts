e91de99015127d292938ff9028c8797d60625866
MAMCB <96302464+MAMCB@users.noreply.github.com>
2024-05-23 16:21:11 +0200

    finished createTopic validation and added database storage save with validation for database errors

--- src/actions/create-topic.ts
+++ src/actions/create-topic.ts
@@ -3,13 +3,19 @@
 import { z } from "zod";
 import {auth} from "@/auth";
 import { CreateTopicFormState } from "@/utils/interfaces";
+import type { Topic } from "@prisma/client";
+import {redirect} from "next/navigation";
+import { db } from "@/db";
+import paths from "@/utils/pathHelpers";
+import { revalidatePath } from "next/cache";
+
 const createTopicSchema = z.object({
   name: z
     .string()
     .min(1)
-    .regex(/^[a-zA-Z][a-zA-Z0-9 -+#]+$/, {
+    .regex(/^[a-z-]+$/, {
       message:
-        "Must start with a letter and contain only letters, numbers, spaces, and -+#",
+        "Must be lowercase letters and hyphens only (no spaces or special characters)",
     }),
   description: z.string().min(10),
 });
@@ -17,27 +23,46 @@ const createTopicSchema = z.object({
 
 
 export async function createTopic (formState: CreateTopicFormState,formData: FormData):Promise<CreateTopicFormState> {
-    const result = createTopicSchema.safeParse({
-        name: formData.get("name") as string,
-        description: formData.get("description") as string,
-    });
-    if(!result.success){
-        
-        return {
-            errors: result.error.flatten().fieldErrors
-        };
-    }
-   const session =await auth();
-   if(!session || !session.user){
+  const result = createTopicSchema.safeParse({
+    name: formData.get("name") as string,
+    description: formData.get("description") as string,
+  });
+  if (!result.success) {
     return {
-        errors: {
-            _form: ["You must be signed in to create a topic"],
-        },
+      errors: result.error.flatten().fieldErrors,
     };
-    }
-    
-    //should revalidate home page
+  }
+  const session = await auth();
+  if (!session || !session.user) {
     return {
-        errors:{}
+      errors: {
+        _form: ["You must be signed in to create a topic"],
+      },
+    };
+  }
+
+  let topic: Topic;
+
+  try {
+    topic = await db.topic.create({
+      data: {
+        slug: result.data.name,
+        description: result.data.description,
+      },
+    });
+  } catch (error: unknown) {
+    if (error instanceof Error) {
+      return {
+        errors: {
+          _form: [error.message],
+        },
+      };
+    } else {
+      return { errors: { _form: ["An unknown error occurred"] } };
     }
+  }
+  //should revalidate home page
+  revalidatePath(paths.home());
+
+  redirect(paths.topicShow(topic.slug));
 };
