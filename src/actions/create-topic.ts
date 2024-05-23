f83b515fe81ffb4f85024dee6c5e828344d3ebbe
MAMCB <96302464+MAMCB@users.noreply.github.com>
2024-05-23 12:34:57 +0200

    created topicCreateForm component and styled homepage layout

--- src/actions/create-topic.ts
+++ src/actions/create-topic.ts
@@ -1,6 +1,6 @@
 'use server';
 
-export async function createTopic (topic:string) {
+export async function createTopic () {
     //should revalidate home page
     
 };
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,15 @@
-
+import TopicCreateForm from "@/components/topics/TopicCreateForm";
 
 export default  function Home() {
  
   //should revalidate every 30 seconds
-  return <div>Home Page</div>;
+  return <div className="grid grid-cols-4 gap-4 p-4">
+    <div className="col-span-3">
+      <h1 className="text-xl m-2">Top posts</h1>
+    </div>
+    <div>
+      <TopicCreateForm />
+    </div>
+
+  </div>;
 }
