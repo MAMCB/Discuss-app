fb1be0de11dcd18874c5776064be0df4b16780de
MAMCB <96302464+MAMCB@users.noreply.github.com>
2024-05-23 15:19:24 +0200

    added validation for createTopicForm with Zod and useFormState

--- src/actions/create-topic.ts
+++ src/actions/create-topic.ts
@@ -1,6 +1,35 @@
 'use server';
 
-export async function createTopic () {
-    //should revalidate home page
+import { z } from "zod";
+import { CreateTopicFormState } from "@/utils/interfaces";
+const createTopicSchema = z.object({
+  name: z
+    .string()
+    .min(1)
+    .regex(/^[a-zA-Z][a-zA-Z0-9 -+#]+$/, {
+      message:
+        "Must start with a letter and contain only letters, numbers, spaces, and -+#",
+    }),
+  description: z.string().min(10),
+});
+
+
+
+export async function createTopic (formState: CreateTopicFormState,formData: FormData):Promise<CreateTopicFormState> {
+    const result = createTopicSchema.safeParse({
+        name: formData.get("name") as string,
+        description: formData.get("description") as string,
+    });
+    if(!result.success){
+        
+        return {
+            errors: result.error.flatten().fieldErrors
+        };
+    }
+   
     
+    //should revalidate home page
+    return {
+        errors:{}
+    }
 };
--- src/components/topics/TopicCreateForm.tsx
+++ src/components/topics/TopicCreateForm.tsx
@@ -1,3 +1,4 @@
+'use client'
 import {
   Input,
   Button,
@@ -7,23 +8,48 @@ import {
   PopoverContent,
 } from "@nextui-org/react";
 import { createTopic } from "@/actions";
+import { useSession } from "next-auth/react";
+import { useFormState } from "react-dom";
+
+
 
 export default function TopicCreateForm() {
+    const session = useSession();
+    const [formState,action]= useFormState(createTopic,{errors:{}});
+
   return (
-    <Popover placement="left">
-      <PopoverTrigger>
-        <Button color="primary">Create Topic</Button>
-      </PopoverTrigger>
-      <PopoverContent>
-        <form action={createTopic}>
-          <div className="flex flex-col gap-4 p-4 w-80">
-            <h3 className="text-lg">Create a topic</h3>
-            <Input label="Name" labelPlacement="outside" placeholder="Name"/>
-            <Textarea label="Description" labelPlacement="outside" placeholder="Describe your topic" />
-            <Button type="submit">Create</Button>
-          </div>
-        </form>
-      </PopoverContent>
-    </Popover>
+    <>
+      {session.data?.user && (
+        <Popover placement="left">
+          <PopoverTrigger>
+            <Button color="primary">Create Topic</Button>
+          </PopoverTrigger>
+          <PopoverContent>
+            <form action={action}>
+              <div className="flex flex-col gap-4 p-4 w-80">
+                <h3 className="text-lg">Create a topic</h3>
+                <Input
+                  name="name"
+                  label="Name"
+                  labelPlacement="outside"
+                  placeholder="Name"
+                  isInvalid={!!formState.errors.name}
+                  errorMessage={formState.errors.name?.join(", ")}
+                />
+                <Textarea
+                  name="description"
+                  label="Description"
+                  labelPlacement="outside"
+                  placeholder="Describe your topic"
+                  isInvalid={!!formState.errors.description}//the double negation (!!) is used to convert the value to a boolean
+                  errorMessage={formState.errors.description?.join(", ")}
+                />
+                <Button type="submit">Create</Button>
+              </div>
+            </form>
+          </PopoverContent>
+        </Popover>
+      )}
+    </>
   );
 }
